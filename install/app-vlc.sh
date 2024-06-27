56b0de20e689b7a268eff29dbc35a46a04b4cc78
flamedfury <heavy.job8629@fastmail.com>
2024-06-27 15:17:23 +1200

    popfury

--- install/app-vlc.sh
+++ install/app-vlc.sh
@@ -1,8 +1,4 @@
-sudo apt update
-sudo apt install -y vlc libavcodec-extra libdvd-pkg
-echo 'libdvd-pkg libdvd-pkg/first-install note' | sudo debconf-set-selections
-echo 'libdvd-pkg libdvd-pkg/post-invoke_hook-active boolean true' | sudo debconf-set-selections
-sudo DEBIAN_FRONTEND=noninteractive dpkg-reconfigure libdvd-pkg
+sudo apt install -y vlc 
 
 # Set VLC as the default
 xdg-mime default vlc.desktop video/x-msvideo
